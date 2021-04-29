3e117e5db7c7174b3cd460ec7ddbd3782e962fee
Kaibiao <kruan5@wisc.edu>
2021-04-29 14:27:23 -0500

    mqtt

--- src/MQTT_Recieve.py
+++ src/MQTT_Recieve.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
 import paho.mqtt.client as mqtt
 
 #Connection success callback
@@ -10,7 +13,28 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic, " : ", int(msg.payload))
     # count = int.from_bytes(msg.payload, "big")  
-    # print(count)     
+    # print(count)   
+    if(msg.topic == "u'esp8266/x_axis'"):
+        cmd_msg.linear.x = int(msg.payload-612)/612
+        cmd_msg.linear.y = 0
+        cmd_msg.linear.z = 0
+        cmd_msg.angular.x = 0
+        cmd_msg.angular.y = 0
+        cmd_msg.angular.z = 0
+        pub_cmd.publish(cmd_msg)
+    elif(msg.topic == "u'esp8266/y_axis'"):
+        cmd_msg.linear.x = 0
+        cmd_msg.linear.y = 0
+        cmd_msg.linear.z = 0
+        cmd_msg.angular.x = 0
+        cmd_msg.angular.y = 0
+        cmd_msg.angular.z = int(msg.payload-612)/612
+        pub_cmd.publish(cmd_msg)
+    
+rospy.init_node('mqtt_bridge', anonymous=True)
+
+pub_cmd = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+cmd_msg = Twist()
 
 client = mqtt.Client()
 
@@ -25,3 +49,6 @@ client.connect('broker.emqx.io', 1883, 60)
 # client.publish('emqtt',payload='Hello World',qos=0)
 
 client.loop_forever()
+
+
+
